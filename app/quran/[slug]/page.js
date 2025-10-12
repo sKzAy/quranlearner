76ccc866683ff6536fa5238dc4a839b073dab6e6
sKzAy <1mkzaexe@gmail.com>
2025-10-12 19:14:48 +0500

    no awaiting for answer while liking maybe fixes

--- app/quran/[slug]/page.js
+++ app/quran/[slug]/page.js
@@ -243,18 +243,13 @@ const Page = ({ params }) => {
                           let numberSurah = Number(number.slug)
                           let verseNumber = verse.id
                           let message = ""
-                          let answer = await onLikeClick(numberSurah, verseNumber, message)
-                          if (answer === 1){
+                           onLikeClick(numberSurah, verseNumber, message)
+                          
                             toast.success("Added to liked verses!", {
                               unstyled: true,
                               className: "bg-green-400 text-white border border-green-400 border-2 px-4 py-2 rounded-md flex gap-2 shadow items-center justify-center",
+                         
                             });
-                          } else if(answer === -1){
-                            toast.success("Error,Try Later!", {
-                              unstyled: true,
-                              className: "bg-red-400 text-white border border-red-400 border-2 px-4 py-2 rounded-md flex gap-2 shadow items-center justify-center",
-                            });
-                          }
                         }}>Skip</AlertDialogCancel>
                         <AlertDialogAction className={"bg-red-500"} onClick={async () => {
                           let numberSurah = Number(number.slug)
