6b6abff869c7e8a356ac94faa0308bcf3ccb4382
sKzAy <1mkzaexe@gmail.com>
2025-10-27 21:01:40 +0500

    added open to mobile navs

--- app/components/mobileNav.js
+++ app/components/mobileNav.js
@@ -8,7 +8,7 @@ const MobileNav = () => {
         <nav className='flex justify-between align-middle items-center md:hidden bg-gradient-to-r from-green-500 to-green-600 text-white
       p-6'>
         <details className="dropdown relative duration-500 animate-in">
-          <summary className="btn m-1 list-none"><ArrowDropDownCircleRounded/></summary>
+          <summary className="btn m-1 list-none text-center"><ArrowDropDownCircleRounded /> Open</summary>
           <ul className="bg-green-700 menu absolute left-0 top-full bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm">
             <li className='shadow p-2'><Link className=' hover:text-black transition-all hover:transition-all duration-500' href={"/search"}><Search /> Search</Link></li>
             <li className='shadow p-2'><Link className=' cursor-pointer hover:text-black transition-all hover:transition-all duration-500' href={'/dashboard'}><SpaceDashboardRounded /> Dashboard</Link></li>
