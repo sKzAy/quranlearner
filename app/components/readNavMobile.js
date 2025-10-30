0f758527db5200da23923a00934e1ddca803ce5c
sKzAy <1mkzaexe@gmail.com>
2025-10-30 09:17:52 +0500

    fixed dashboard

--- app/components/readNavMobile.js
+++ app/components/readNavMobile.js
@@ -10,7 +10,7 @@ const ReadNavMobile = () => {
             <summary className="btn m-1 list-none text-center"><ArrowDropDownCircleRounded /></summary>
             <ul className="bg-green-700 menu absolute left-0 top-full bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm">
               <li className='shadow p-2'><Link className=' hover:text-black transition-all hover:transition-all duration-500' href={"/choose"}> <AutoStories /> Select Surah</Link></li>
-              <li className='shadow p-2'><Link className=' cursor-pointer hover:text-black transition-all hover:transition-all duration-500' href={'/dashboard'}>Dashboard <SpaceDashboardRounded /></Link></li>
+              <li className='shadow p-2'><Link className=' cursor-pointer hover:text-black transition-all hover:transition-all duration-500' href={'/dashboard'}><SpaceDashboardRounded /> Dashboard </Link></li>
             </ul>
           </details>
           <div className='text-2xl font-bold flex gap-2'>Quran Reader</div>
